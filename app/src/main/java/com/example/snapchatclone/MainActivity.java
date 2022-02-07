2bc39d9a9b36050f3ccc61dd26c55a34da2aeba0
choklangpc <choklangpc@outlook.com>
2022-02-07 10:41:08 -0500

    test1

---
+++ app/src/main/java/com/example/snapchatclone/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.snapchatclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //crate and get elements
+
+        //TabLayout tabLayout=findViewById(R.id.tabs);
+        //add to main activity xml if I want add tabs
+        /** <com.google.android.material.tabs.TabLayout
+         android:id="@+id/tabs"
+         android:layout_width="match_parent"
+         android:layout_height="wrap_content">
+
+         </com.google.android.material.tabs.TabLayout>
+         also add android:layout_below="@+id/tabs" to viewpager2 **/
+
+        ViewPager2 viewPager2;
+        viewPager2= findViewById(R.id.viewPager2);
+        ViewPageAdapter adapter= new ViewPageAdapter(this);
+        viewPager2.setAdapter(adapter);
+        //below make tabs move tab1,tab2,tab3
+        /** new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Tab"+(position +1));
+            }
+        }).attach(); **/
+
+    }
+
+}
